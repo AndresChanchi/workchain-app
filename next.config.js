d8ea5333a9a423325a1a7957d4f3cbb35ce570e1
Andres Chanchi <4chanchi@gmail.com>
2025-03-25 16:33:05 -0500

    chore[config]: update next.config.js, postcss.config.js, package.json, and package-lock.json with latest settings

--- next.config.js
+++ next.config.js
@@ -4,7 +4,33 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true }
+  images: { 
+    unoptimized: true 
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+  env: {
+    NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'unsafe-none', // Necesario para Coinbase Wallet SDK
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'credentialless', // Ayuda a evitar bloqueos
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
